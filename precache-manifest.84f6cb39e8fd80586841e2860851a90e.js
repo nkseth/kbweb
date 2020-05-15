d37211a3611cdf341332d330dfb3ef7292b0d2fa
nksdevelopers <nanaksethh@gmail.com>
2020-05-15 17:03:05 +0530

    Updates

---
+++ precache-manifest.84f6cb39e8fd80586841e2860851a90e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f4b846dce2738f679d2f113cc6cf54",
+    "url": "/kbweb/index.html"
+  },
+  {
+    "revision": "da0b1e922ea4857e25ac",
+    "url": "/kbweb/static/css/2.8327a0ae.chunk.css"
+  },
+  {
+    "revision": "377e2331312f899ffca6",
+    "url": "/kbweb/static/css/main.5d34a109.chunk.css"
+  },
+  {
+    "revision": "da0b1e922ea4857e25ac",
+    "url": "/kbweb/static/js/2.d53d5d3a.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/kbweb/static/js/2.d53d5d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377e2331312f899ffca6",
+    "url": "/kbweb/static/js/main.e39d3bcc.chunk.js"
+  },
+  {
+    "revision": "4ca3894384d7e272ca9f",
+    "url": "/kbweb/static/js/runtime-main.24e13a9b.js"
+  },
+  {
+    "revision": "a1f84c90db7a132e76f89ccc923fb856",
+    "url": "/kbweb/static/media/f1.a1f84c90.jpg"
+  },
+  {
+    "revision": "caf9a69078d2abbe3446d9e9ee276352",
+    "url": "/kbweb/static/media/ppk.caf9a690.jpg"
+  }
+]);
